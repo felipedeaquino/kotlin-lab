b75b69b19eb2286b8a49a1d96ef6e0309482502c
Felipe <felipe.puziol@gmail.com>
2024-02-24 16:44:02 -0300

    feat: implements Usuario class

--- desafio.kt
+++ desafio.kt
@@ -2,7 +2,37 @@
 
 enum class Nivel { BASICO, INTERMEDIARIO, AVANCADO }
 
-class Usuario
+class Usuario private constructor(
+  val id: Int,
+  var nome: String,
+  val email: String,
+) {
+    var nivel: Nivel = Nivel.BASICO
+      private set
+
+    companion object {
+      private var contadorIds = 1
+
+      fun criarUsuario(nome: String, email: String, nivel: Nivel = Nivel.BASICO): Usuario {
+        if (nome.isBlank() || email.isBlank()) {
+          throw IllegalArgumentException("Nome e email não podem estar vazios.")
+        }
+        val id = contadorIds++
+        return Usuario(id, nome, email).apply { this.nivel = nivel }
+      }
+    }
+
+    fun alterarNivel(novoNivel: Nivel) {
+      nivel = novoNivel
+      println("Novo nível do Usuário de matrícula ${this.id}, ${this.nome}: ${this.nivel}.")
+    	println()
+    }
+    fun alterarNome(novoNome: String) {
+      nome = novoNome
+      println("Nome do Usuário de matrícula ${this.id} alterado para $novoNome.")
+      println()
+    }
+}
 
 data class ConteudoEducacional(val nome: String, val duracao: Int = 60)
 
